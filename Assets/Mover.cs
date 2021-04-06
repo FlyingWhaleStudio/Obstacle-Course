30342012ed493759f61e2cce09eb829c2473e318
FlyingWhale <flyingwhalestudio@icloud.com>
2021-04-06 19:25:25 +0200

    Refactored Mover.cs and created ObjectHit.cs to handle collisions.

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -9,15 +9,27 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
         transform.Translate(xValue, 0, zValue);
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("This is an obstacle course");
+        Debug.Log("Use Z, Q, S, D or arrow keys to find yout way out");
     }
 }
