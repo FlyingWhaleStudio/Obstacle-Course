170416c373ea2a8dffbadefbe68bcfea593a1989
FlyingWhale <flyingwhalestudio@icloud.com>
2021-04-07 18:22:20 +0200

    Added access to Rigidbody and Meshrenderer in Dropper.cs

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
-    [SerializeField] float dropTime = 0f;
+    new MeshRenderer renderer;
+    new Rigidbody rigidbody;
+
+    [SerializeField] float timeToWait = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = GetComponent<MeshRenderer>();
+        rigidbody = GetComponent<Rigidbody>();
+
+        renderer.enabled = false;
+        rigidbody.useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.time > dropTime)
+        if(Time.time > timeToWait)
         {
-            Debug.Log(dropTime + " seconds has elapsed");
+            renderer.enabled = true;
+            rigidbody.useGravity = true;
         }
     }
 }
