23789bbe21d86fec54dd7989f106ff8a14d3a181
FlyingWhale <flyingwhalestudio@icloud.com>
2021-04-11 19:13:22 +0200

    Added cascading colors for ramps

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
+    [SerializeField] GameObject other;
+    MeshRenderer renderer1;
+    MeshRenderer renderer2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer1 = GetComponent<MeshRenderer>();
+        renderer2 = other.GetComponent<MeshRenderer>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            GetComponent<MeshRenderer>().material.color = Color.red;
+            renderer1.material.color = Color.red;
+            renderer2.material.color = Color.red;
             gameObject.tag = "Hit";
         }
     }
